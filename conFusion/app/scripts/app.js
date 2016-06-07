15e15d63ac1710cac48ed681c74bfd69ee6efc0d
Marcos de la Calle <mcsamaniego.rs@gmail.com>
2016-06-07 20:42:06 +0200

    Grunt integration

---
+++ conFusion/app/scripts/app.js
@@ -0,0 +1,72 @@
+/**
+ * Created by marcos on 7/06/16.
+ */
+
+'use strict';
+
+var app = angular.module('confusionApp', []);
+
+app.controller("menuController", function () {
+
+    var vm = this;
+    vm.tab = 1;
+    vm.dishes = [
+        {
+            name: 'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comment: ''
+        },
+        {
+            name: 'Zucchipakoda',
+            image: 'images/zucchipakoda.png',
+            category: 'appetizer',
+            label: '',
+            price: '1.99',
+            description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+            comment: ''
+        },
+        {
+            name: 'Vadonut',
+            image: 'images/vadonut.png',
+            category: 'appetizer',
+            label: 'New',
+            price: '1.99',
+            description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+            comment: ''
+        },
+        {
+            name: 'ElaiCheese Cake',
+            image: 'images/elaicheesecake.png',
+            category: 'dessert',
+            label: '',
+            price: '2.99',
+            description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+            comment: ''
+        }
+    ];
+    vm.filtText = '';
+
+    vm.select = function (setTab) {
+        this.tab = setTab;
+
+        if (setTab === 2) {
+            this.filtText = "appetizer";
+        }
+        else if (setTab === 3) {
+            this.filtText = "mains";
+        }
+        else if (setTab === 4) {
+            this.filtText = "dessert";
+        }
+        else {
+            this.filtText = "";
+        }
+    };
+    vm.isSelected = function (checkTab) {
+        return (this.tab === checkTab);
+    };
+});
