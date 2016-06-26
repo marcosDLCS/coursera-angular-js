59541d76811357ca7b71a9300e7ef011ecafbb82
Marcos de la Calle <mcsamaniego.rs@gmail.com>
2016-06-26 13:35:13 +0200

    Yo and Yeoman

---
+++ angular-example/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularExampleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularExampleApp
+ */
+angular.module('angularExampleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ angular-example/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularExampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularExampleApp
+ */
+angular.module('angularExampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
